1933ef32ab6252d749894d8ac8b711772c161453
Guillaume Mayer <gmayer@arkho.tech>
2017-02-01 18:30:28 -0300

    Run script improved

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 docker run \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e SLACK_WEB_HOOK=https://hooks.slack.com/services/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 \
+    -d \
+    --name slack-docker-events \
     slack-docker-events
